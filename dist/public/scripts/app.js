709caf361739a6873839b14d2e51b10f29b36b98
Aileen <asantos3026@gmail.com>
2017-04-16 20:36:41 -0700

    Script file updated

--- dist/public/scripts/app.js
+++ dist/public/scripts/app.js
@@ -1,1 +1,20 @@
-undefined
+'use strict';
+
+$(function () {
+  window.addEventListener('scroll', function (event) {
+    setOverlay(window.pageYOffset);
+  });
+
+  function setOverlay(yOffset) {
+    var navBar = document.getElementsByTagName('nav')[0];
+    if (yOffset > 0) {
+      navBar.classList.add('overlay');
+    } else {
+      navBar.classList.remove('overlay');
+    }
+  }
+});
+
+// $(document).on('scroll', function (e) {
+//     $('.navbar').css('opacity', ($(document).scrollTop() / 500));
+// })
