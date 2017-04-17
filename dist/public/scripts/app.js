4e25ea9d43a4dd1110d59c9c73e67605cce9ba6c
Aileen <asantos3026@gmail.com>
2017-04-16 19:17:22 -0700

    Navbar full opacity on scroll

---
+++ dist/public/scripts/app.js
@@ -0,0 +1,19 @@
+$( () => {
+  window.addEventListener('scroll', (event) => {
+    setOverlay(window.pageYOffset)
+  })
+
+  function setOverlay(yOffset) {
+    const navBar = document.getElementsByTagName('nav')[0]
+    if(yOffset > 0) {
+      navBar.classList.add('overlay')
+    } else {
+      navBar.classList.remove('overlay')
+    }
+  }
+})
+
+// $(document).on('scroll', function (e) {
+//     $('.navbar').css('opacity', ($(document).scrollTop() / 500));
+// })
+
