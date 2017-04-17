e934ad20b86f90db34b84d319bcfcc99f76886c9
Aileen <asantos3026@gmail.com>
2017-04-16 19:31:34 -0700

    Fix heroku

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,14 +1,8 @@
-'use strict';
+import express from 'express';
 
-var _express = require('express');
+const router = express.Router();
 
-var _express2 = _interopRequireDefault(_express);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var router = _express2.default.Router();
-
-router.get('/', function (request, response) {
+router.get('/', (request, response) => {
   response.render('home');
 });
 
--- dist/server.js
+++ dist/server.js
@@ -1,37 +1,19 @@
-'use strict';
+import express from 'express';
+import index from './routes/index';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
 
-var _express = require('express');
-
-var _express2 = _interopRequireDefault(_express);
-
-var _index = require('./routes/index');
-
-var _index2 = _interopRequireDefault(_index);
-
-var _path = require('path');
-
-var _path2 = _interopRequireDefault(_path);
-
-var _morgan = require('morgan');
-
-var _morgan2 = _interopRequireDefault(_morgan);
-
-var _bodyParser = require('body-parser');
-
-var _bodyParser2 = _interopRequireDefault(_bodyParser);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var server = (0, _express2.default)();
+const server = express();
 
 //middleware
-server.set('views', _path2.default.join(__dirname, 'views'));
+server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'ejs');
-server.use((0, _morgan2.default)('dev'));
-server.use(_express2.default.static(_path2.default.join(__dirname + '/public')));
-server.use(_bodyParser2.default.urlencoded({ extended: true }));
+server.use(logger('dev'));
+server.use(express.static(path.join(__dirname + '/public')));
+server.use(bodyParser.urlencoded({ extended: true }));
 
-server.use('/', _index2.default);
+server.use('/', index);
 
 server.listen(process.env.PORT || 4000);
 
