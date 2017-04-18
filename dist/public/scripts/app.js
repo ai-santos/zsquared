acc5dbbb41ac86bfec36ca1d2ccdbea109ee6b24
Aileen <asantos3026@gmail.com>
2017-04-17 21:59:06 -0700

    WIP center logos except, rebuilding poster

--- dist/public/scripts/app.js
+++ dist/public/scripts/app.js
@@ -1,19 +1,6 @@
 'use strict';
 
 $(function () {
-  window.addEventListener('scroll', function (event) {
-    setOverlay(window.pageYOffset);
-  });
-
-  function setOverlay(yOffset) {
-    var navBar = document.getElementsByTagName('nav')[0];
-    if (yOffset > 0) {
-      navBar.classList.add('overlay');
-    } else {
-      navBar.classList.remove('overlay');
-    }
-  }
-
   var slides = document.querySelectorAll('.slides .slide');
   var currentSlide = 0;
   var slideInterval = setInterval(nextSlide, 5000);
--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,17 +1,4 @@
 $( () => {
-  window.addEventListener('scroll', (event) => {
-    setOverlay(window.pageYOffset)
-  })
-
-  function setOverlay(yOffset) {
-    const navBar = document.getElementsByTagName('nav')[0]
-    if(yOffset > 0) {
-      navBar.classList.add('overlay')
-    } else {
-      navBar.classList.remove('overlay')
-    }
-  }
-
   let slides = document.querySelectorAll('.slides .slide');
   let currentSlide = 0;
   let slideInterval = setInterval(nextSlide, 5000);
